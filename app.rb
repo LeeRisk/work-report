135116e079dc335d9f98b19ebcc30160e64f52e4
Guojian <=>
2013-05-05 23:58:11 +0800

    modify app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,5 @@
 require 'sinatra'
-<<<<<<< HEAD
 require 'sinatra/json'
-=======
-require 'sinatra/base'
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
 require 'mongoid'
 require 'haml'
 require './models'
@@ -16,11 +12,8 @@ before do
   unless request.path_info == '/login'
     redirect '/login' if session[:auth].nil?
   end
-<<<<<<< HEAD
   
   @auth = session[:auth]
-=======
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
 end
 
 get '/assets/:static_file' do
@@ -52,25 +45,14 @@ end
 
 # => To /
 get '/' do
-<<<<<<< HEAD
-=======
-  @auth       = session[:auth]
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
   @last_rep   = Report.where(:team => @auth.team, :order => Report.max(:order)).first
   @today_plan = ReportItem.where(:author => @auth, :date => Date.today).first
   haml :index
 end
 
-<<<<<<< HEAD
 get '/teamview/?:order?' do |order|
   max_order     = Report.max(:order)
   @order        = (order || max_order).to_i
-=======
-get '/teamview' do
-  @auth         = session[:auth]
-  max_order     = Report.max(:order)
-  @order        = (params[:order] || max_order).to_i
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
   @last_rep     = Report.where(:team => @auth.team, :order => @order).first
   @reps_by_date = Report.sort_and_group_by_date(@last_rep)
   @has_next     = @order < max_order
@@ -79,7 +61,6 @@ get '/teamview' do
 end
 
 # => Process request of add team
-<<<<<<< HEAD
 post '/team/new' do
   begin
     @team = Team.create params[:team]
@@ -96,43 +77,21 @@ end
 
 # => Process request of add memebers for a ateam
 post '/member/new' do
-=======
-post '/team/add' do
-  Team.create params[:team]
-  redirect '/'
-end
-
-# => Process request of add memebers for a ateam
-post '/members/add' do
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
   User.create params[:user]
   redirect '/'
 end
 
 # => Process request of edit of add a reportitem to given report
-<<<<<<< HEAD
 get '/report/edit/:date/:repid' do |date,repid|
   # debugger
   @date = date
   begin
     @report = Report.find(repid)
-=======
-get '/report/edit/:date/:repid' do
-  # debugger
-  @auth = session[:auth]
-  @date = params[:date]
-  begin
-    @report = Report.find(params[:repid])
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
   rescue
     redirect '/' if @report.nil?
   end
 
-<<<<<<< HEAD
   @reportItem = ReportItem.where(:report => @report, :date => date, :author => @auth).first
-=======
-  @reportItem = ReportItem.where(:report => @report, :date => @date, :author => @auth).first
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
   if @reportItem.nil?
     haml :itemnew
   else
@@ -159,10 +118,6 @@ post '/reportitem/edit' do
 end
 
 get '/report/new' do
-<<<<<<< HEAD
-=======
-  @auth  = session[:auth]
->>>>>>> 50c47b082ce838e7a4b047a02c1cf37636d64f35
   @order = Report.max(:order).to_i + 1
   haml :report_new
 end
